14850baf9252c3cc1687ee97baa781879c45d4df
JacedaAce67 <jace.karpowecz748@students.cmcss.net>
2025-12-17 11:50:32 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
+slices_of_pizza = (large + medium + people)
+total = party_pizza_mini/people
 
+people = +1
+
+
+# TODO Print share result
+# TODO print leftover result
+
+# TODO Update people #Eric and Brandon are coming too.
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Print share results
+# TODO Print leftover result
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+# TODO Update slices
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Print share result
+# TODO Print leftover result
+print("...for Mr. Hollow Leg")
